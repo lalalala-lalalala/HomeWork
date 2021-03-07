d3602ba8bbf434f32ffb865d6f6b51f0a7b89d75
lalalala-lalalala <79784145+lalalala-lalalala@users.noreply.github.com>
2021-03-07 11:47:43 +0800

    Add files via upload

---
+++ HomeWork2/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class M_N
+    {
+        public bool If_is_Toeplize(int [,] mn)
+        {
+            int min;
+            int num = mn[0,0];
+            bool b = true;
+            if (mn.GetLength(0) > mn.GetLength(1))
+            {
+                min = mn.GetLength(1);
+            }
+            else
+            {
+                min = mn.GetLength(0);
+            }
+            for(int i = 0; i < min; i++)
+            {
+                if (mn[i, i] != num)
+                {
+                    b = false;
+                    break;
+                }
+            }
+            return b;
+        }
+    }
+}
