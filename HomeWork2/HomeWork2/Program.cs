0762b1e0d548411913062313ac84aead912eacae
lalalala-lalalala <79784145+lalalala-lalalala@users.noreply.github.com>
2021-03-07 11:47:00 +0800

    Add files via upload

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str;
+            int num;
+            str = Console.ReadLine();
+            num = int.Parse(str);
+            int t = num;
+            int i, j = 0;
+            for (i = 2; i <= Math.Abs(num); i++)
+            {
+                int k = (int)Math.Sqrt(i);
+                for (j = 2; j <= k; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        break;
+                    }
+                }
+
+                if (j > k)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.Write(i.ToString() + "   ");
+                    }
+                }
+            }
+        }
+    }
+}
